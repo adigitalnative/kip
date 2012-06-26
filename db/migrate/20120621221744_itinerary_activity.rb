7c5fbc1393e02665c40d21f437e7fa4e4de7e0a5
ADigitalNative <adigitalnative@gmail.com>
2012-06-26 09:43:57 -0400

    Cleanup

--- db/migrate/20120621221744_itinerary_activity.rb
+++
@@ -1,8 +0,0 @@
-class ItineraryActivity < ActiveRecord::Migration
-  def change
-    create_table :itinerary_activity do |t|
-      t.integer :itinerary_id
-      t.integer :activity_id      
-    end
-  end
-end
