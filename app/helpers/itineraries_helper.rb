6f52e85448b606a6589243ab55db86a52433abd1
ADigitalNative <adigitalnative@gmail.com>
2012-07-02 17:48:36 -0400

    Itineraries views as series of divs, not a table

--- app/helpers/itineraries_helper.rb
+++ app/helpers/itineraries_helper.rb
@@ -4,4 +4,28 @@ module ItinerariesHelper
     Itinerary.find(itinerary_id).activities.delete(Activity.find(activity_id))
   end
 
+  def itinerary_photo_url(id)
+    activities = Itinerary.find(id).activities
+    deal_activity = nil
+    activities.each do |activity|
+      if activity.deal == true
+        deal_activity = activity
+      end
+    end
+    if deal_activity
+      deal_activity.image_url
+    end
+  end
+
+  def deal_name(itinerary_id)
+    activities = Itinerary.find(itinerary_id).activities
+    deal_activity = nil
+    activities.each do |activity|
+      if activity.deal == true
+        deal_activity = activity
+      end
+    end
+    deal_activity
+  end
+
 end
