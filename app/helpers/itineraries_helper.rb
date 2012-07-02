8ccf771da099b149d511d3600f2f17f105fa6f67
ADigitalNative <adigitalnative@gmail.com>
2012-07-02 11:46:15 -0400

    Minor tweaks

--- app/helpers/itineraries_helper.rb
+++ app/helpers/itineraries_helper.rb
@@ -3,4 +3,5 @@ module ItinerariesHelper
   def delete_activity_from_itinerary(itinerary_id, activity_id)
     Itinerary.find(itinerary_id).activities.delete(Activity.find(activity_id))
   end
+
 end
